80534f0e5befbef6dd7bb4289c997c660e1dc53f
Derean Simmons <dereansim1@gmail.com>
2026-08-26 16:42:19 -0400

    Complete exercise 5

--- src/exercise05.ts
+++ src/exercise05.ts
@@ -1,9 +1,15 @@
-export type NetworkConfig = {};
+export type NetworkConfig = {serverUrl: string; port: number};
 
-export type EnvironmentConfig = {};
+export type EnvironmentConfig = {environment: "dev" | "prod"; timeout: number};
 
 export type AppConfig = NetworkConfig & EnvironmentConfig;
 
 export function initializeConfig(userOverrides: Partial<AppConfig>): AppConfig {
-  return {};
+  return {
+    serverUrl: "http://localhost",
+    port: 8080,
+    environment: "dev",
+    timeout: 3000,
+    ...userOverrides
+  };
 }
