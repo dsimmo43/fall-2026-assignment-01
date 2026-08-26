714bf847390a6325e839948dd5309fbbe9a4531b
Derean Simmons <dereansim1@gmail.com>
2026-08-26 16:59:17 -0400

    Complete exercise 7

--- src/exercise07.ts
+++ src/exercise07.ts
@@ -1,3 +1,24 @@
+import fs from "fs";
+
+export type Gradebook = {
+  [student: string]: {
+    [subject: string]: number;
+  };
+};
+
 export function calculateSubjectAverage(subject: string): number {
-  return 0;
+  const data = fs.readFileSync("data/gradebook.json", "utf-8");
+  const gradebook: Gradebook = JSON.parse(data);
+
+  const grades: number[] = [];
+  for (const student in gradebook) {
+    if (subject in gradebook[student]) {
+      grades.push(gradebook[student][subject]);
+    }
+  }
+
+  if (grades.length === 0) {
+    return 0;
+  }
+  return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
 }
