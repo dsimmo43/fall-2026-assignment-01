ba6f1ef6a71d16916caebfa71b2c5608771758a9
Derean Simmons <dereansim1@gmail.com>
2026-08-31 12:06:29 -0400

    Complete exercise 11

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,11 @@
+import fs from 'fs/promises';
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const timeStamp = new Date().toISOString();
+  const message = `${statusMessage} - ${timeStamp}\n`;
+
+  await fs.appendFile(filePath, message);
+}
