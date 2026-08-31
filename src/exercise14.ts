b1d0250ba8a8c720211dc7cb24da9120cdfd8108
Derean Simmons <dereansim1@gmail.com>
2026-08-31 16:10:31 -0400

    Complete exercise 14

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,16 @@
-export type PostItem = {};
+import fs from 'fs/promises';
+
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const requests = postIds.map((id) => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`));
+
+  const responses = await Promise.all(requests);
+
+  const posts: PostItem[] = await Promise.all(responses.map((response) => response.json()));
+  return posts;
 }
